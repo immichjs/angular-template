94a7dadb0b933608777d0b61c91e0f9f4fa59d60
Michel França <fr.mich17@gmail.com>
2024-12-19 04:19:26 -0300

    feat: inicialização do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { JsonPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [JsonPipe],
 })
-export class AppComponent {
-  title = 'angular-boilerplate';
+export class AppComponent implements OnInit {
+  title: string = '';
+  token: string = '';
+
+  ngOnInit(): void {
+    const eventSource = new EventSource(
+      'http://localhost:3000/notification/listener'
+    );
+    eventSource.addEventListener('message', (event: MessageEvent) => {
+      this.title = JSON.parse(event.data);
+    });
+  }
 }
